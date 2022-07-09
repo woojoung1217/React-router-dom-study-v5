fbf850114fb88fbf414be98476f1f1315c56d923
woojoung <ywj981217@naver.com>
2022-07-09 18:23:22 +0900

    test moduel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
@@ -7,13 +7,17 @@ import NotFound from "./pages/NotFound";
 import Links from "./components/Links";
 import NavLinks from "./components/NavLinks";
 import Login from "./pages/Login";
+
+
+const isLogin = true;
+
 function App() {
   return (
     <BrowserRouter>
       <Links/>
       <NavLinks/>
       <Switch>
-        <Route path="/login" component={Login}></Route>
+        <Route path="/login" render={()=> (isLogin ? <Redirect to="/" />:<Login/>)}/>
         <Route path="/profile/1" component={Profile}></Route>
         <Route path="/profile" component={Profile}></Route>
         <Route path="/about" component={About}></Route>
